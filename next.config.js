7bf5a7453b487b6aa755eb9b82a0dba676c0588a
moyomogi <tsuyomogi@gmail.com>
2023-09-08 23:58:26 +0900

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import("next").NextConfig} */
 
-require("dotenv").config();
-let branchName = "/web-koushu";
+// require("dotenv").config();
+// let branchName = "/web-koushu";
 
 const nextConfig = {
-  output: process.env.BUILD_TYPE == "test" ? "" : "export",
-  assetPrefix: process.env.BUILD_TYPE == "test" ? "" : branchName,
-  basePath: process.env.BUILD_TYPE == "test" ? "" : branchName,
-  trailingSlash: true,
+  // output: process.env.BUILD_TYPE == "test" ? "" : "export",
+  // assetPrefix: process.env.BUILD_TYPE == "test" ? "" : branchName,
+  // basePath: process.env.BUILD_TYPE == "test" ? "" : branchName,
+  // trailingSlash: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 };
 
 module.exports = nextConfig;
