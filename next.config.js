b8397d420514f5f17c860466f383d1c846ab3e1c
moyomogi <tsuyomogi@gmail.com>
2023-09-08 23:30:25 +0900

    Fix nextjs.yaml

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 let branchName = "web-koushu";
 
 const nextConfig = {
-  output: process.env.BUILD_TYPE == "production" ? "export" : "",
-  assetPrefix: process.env.BUILD_TYPE == "production" ? branchName : "",
-  basePath: process.env.BUILD_TYPE == "production" ? branchName : "",
+  output: process.env.BUILD_TYPE == "test" ? "" : "export",
+  assetPrefix: process.env.BUILD_TYPE == "test" ? "" : branchName,
+  basePath: process.env.BUILD_TYPE == "test" ? "" : branchName,
   trailingSlash: true,
 };
 
