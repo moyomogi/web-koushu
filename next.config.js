120fc3a125fb0e1331c3b9f57694c214fe610ece
moyomogi <tsuyomogi@gmail.com>
2023-09-08 21:29:06 +0900

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export",
+  basePath: "web-koushu",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
