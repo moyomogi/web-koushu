36f6f53f080a19115b990b38432c5606f15e1725
moyomogi <tsuyomogi@gmail.com>
2023-09-08 23:14:14 +0900

    Fix layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "simple-icons-font/font/simple-icons.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hello World",
@@ -18,7 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className + " bg-pink-50"}>
+      {/* <body className={inter.className + " bg-pink-50"}> */}
+      <body className="bg-pink-50">
         <Header />
         <main className="min-h-screen">{children}</main>
         <footer className="text-center p-5">
